89e5663b96a612750525c9922b2e11c9feaf6834
Aleksandr Sirik <Asirik9459@conestogac.on.ca>
2021-10-17 05:07:50 +0300

    One side (width) was adde to getPeremiter function, If statements were corrected (input<=100 and input>=0 were changed into input<100 and input>0)

---
+++ GetPerimeter_Test/GetPerimeter_Test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GetPerimeterTest
+{
+	TEST_CLASS(GetPerimeterTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ GetRectanlePerimeter_Test/GetRectanlePerimeter_Test.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GetRectanlePerimeterTest
+{
+	TEST_CLASS(GetRectanlePerimeterTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
